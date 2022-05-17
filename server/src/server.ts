0e72e53b45315f0911df640877a942f68c62baa7
DefepDaelt <claudio.roberto.santana.f@gmail.com>
2022-05-17 18:22:02 -0300

    fix(server): justfy start server massage

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,10 @@
 import { app } from './app'
 
-app.listen(process.env.PORT || 3333, () => {
-  console.log('Server is running in http://localhost:3333/')
+const ENVIRONMENT = process.env.RAILWAY_ENVIRONMENT || 'developmemnt'
+const PROTOCOLL = ENVIRONMENT === 'production' ? 'https://' : 'http://'
+const HOST = process.env.RAILWAY_STATIC_URL || 'localhost'
+const PORT = process.env.PORT || 3333
+
+app.listen(PORT, () => {
+  console.log(`Server is running in ${PROTOCOLL}${HOST}${ENVIRONMENT === 'developmemnt' && `:${PORT}`}`)
 })
